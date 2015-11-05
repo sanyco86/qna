e8eba8e3fcb71d6124a9ef88a2dd6632a4b3d0e8
Lutsko Michael <lutsko86@gmail.com>
2015-11-05 10:40:46 +0200

    Spec clean 'RSpec. type: :model ' for describe Model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Answer, type: :model do
+describe Answer do
 
   it { should validate_presence_of :body }
   it { should belong_to :question }
   it { should belong_to :user }
-
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Question, type: :model do
+describe Question do
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should have_many(:answers).dependent(:destroy) }
   it { should belong_to :user }
-
 end
