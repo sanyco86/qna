1aed5f28a21722c8d791a72c2c0a8b533320aadd
Lutsko Michael <lutsko86@gmail.com>
2015-11-17 16:52:41 +0200

    validates file

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,4 +2,6 @@ class Attachment < ActiveRecord::Base
   belongs_to :attachmentable, polymorphic: true
 
   mount_uploader :file, AttachmentUploader
+
+  validates :file, presence: true
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -2,7 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
   it { should belong_to :attachmentable }
-
   it { should validate_presence_of :file }
-  it { should validate_presence_of :attachmentable }
 end
