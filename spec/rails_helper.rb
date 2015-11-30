3b3ada4820534c91ed2439eb88000e8a13024f0c
Michael Lutsko <lutsko86@gmail.com>
2015-11-30 20:18:12 +0200

    fix rspec helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,10 +29,7 @@ RSpec.configure do |config|
   config.include FeatureMacros, type: :feature
   config.include OmniauthMacros
 
-
-  config.fixture_path = "#{::Rails.root}/spec/features"
-
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-
-
 end
