b3453772f36e74357ad8d09c80ed264aa6195e96
Lutsko Michael <lutsko86@gmail.com>
2015-12-02 08:49:51 +0200

    update  lurker  html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   use_doorkeeper
-  namespace :api do
+  namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :profiles do
         get :me, on: :collection
