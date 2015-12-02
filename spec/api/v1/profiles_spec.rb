2a90c29afc4982225bcdb129e2dc4fc1f5e9189b
Lutsko Michael <lutsko86@gmail.com>
2015-12-02 16:22:46 +0200

    fix profiles_spec

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -1,134 +1,58 @@
 require 'rails_helper'
 
-describe 'Answers API' do
-  describe 'GET /index' do
-    let(:question) { create(:question) }
-
+describe 'Profiles API' do
+  describe 'GET /' do
     it_behaves_like 'Api Authenticable'
 
     context 'authorized', :lurker do
-      let(:access_token) { create(:access_token) }
-      let!(:answers) { create_list(:answer, 3, question: question) }
-      let(:answer) { answers.first }
+      let(:me) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+      let!(:users) { create_list(:user, 4) }
 
-      before { get "/api/v1/questions/#{question.id}/answers", format: :json, access_token: access_token.token }
+      before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
 
       it_behaves_like 'successfully reponsible'
 
-      it 'returns list of answers' do
-        expect(response.body).to have_json_size(3).at_path('answers')
+      it 'contains array of users' do
+        expect(response.body).to be_json_eql(users.to_json).at_path('profiles')
       end
 
-      %w(id body created_at updated_at question_id).each do |attr|
-        it "answer object contains #{attr}" do
-          expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("answers/0/#{attr}")
-        end
+      it 'doesnt contain current user' do
+        expect(response.body).to_not include_json(me.to_json)
       end
     end
 
     def do_request(options = {})
-      get "/api/v1/questions/#{question.id}/answers", { format: :json }.merge(options)
+      get '/api/v1/profiles', { format: :json }.merge(options)
     end
   end
 
-  describe 'GET /show' do
-    let(:answer) { create(:answer) }
-    let(:access_token) { create(:access_token) }
-    let!(:comment) { create(:comment, commentable: answer) }
-    let!(:attachment) { create(:attachment, attachmentable: answer) }
-
+  describe 'GET /me' do
     it_behaves_like 'Api Authenticable'
 
     context 'authorized', :lurker do
-      before { get "/api/v1/answers/#{answer.id}", format: :json, access_token: access_token.token }
+      let(:me) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
 
       it_behaves_like 'successfully reponsible'
 
-      it 'return answer object' do
-        expect(response.body).to have_json_size(1)
-      end
-
-      %w(id body created_at updated_at question_id).each do |attr|
-        it "answer object contains #{attr}" do
-          expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("answer/#{attr}")
+      %w(id email).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
         end
       end
 
-      context 'comments' do
-        it 'includes comments list' do
-          expect(response.body).to have_json_size(1).at_path('answer/comments')
-        end
-
-        %w(id body created_at updated_at).each do |attr|
-          it "comment object contains #{attr}" do
-            expect(response.body).to be_json_eql(comment.send(attr.to_sym).to_json).at_path("answer/comments/0/#{attr}")
-          end
-        end
-      end
-
-      context 'attachments' do
-        it 'includes attachments list' do
-          expect(response.body).to have_json_size(1).at_path('answer/attachments')
-        end
-
-        it 'attachment object contains url' do
-          expect(response.body).to be_json_eql(attachment.url.to_json).at_path('answer/attachments/0/url')
+      %w(password encrypted_password).each do |attr|
+        it "doesnt contain #{attr}" do
+          expect(response.body).to_not have_json_path(attr)
         end
       end
     end
 
     def do_request(options = {})
-      get "/api/v1/answers/#{answer.id}", { format: :json }.merge(options)
-    end
-  end
-
-  describe 'POST /create' do
-    let(:access_token) { create(:access_token) }
-    let(:user) { User.find(access_token.resource_owner_id) }
-    let(:question) { create(:question, user: user) }
-
-    it_behaves_like 'Api Authenticable'
-
-    context 'authorized' do
-      context 'with valid attributes', :lurker do
-        subject { post "/api/v1/questions/#{question.id}/answers", format: :json, access_token: access_token.token, answer: attributes_for(:answer) }
-
-        it 'reponses with 201' do
-          subject
-          expect(response.status).to eq 201
-        end
-
-        it 'creates new answer' do
-          expect{
-            subject
-          }.to change(user.answers, :count).by 1
-        end
-
-        it 'creates new answer' do
-          expect{
-            subject
-          }.to change(question.answers, :count).by 1
-        end
-      end
-
-      context 'with invalid attributes' do
-        subject { post "/api/v1/questions/#{question.id}/answers", format: :json, access_token: access_token.token, answer: { body: nil } }
-
-        it 'responses with 422' do
-          subject
-          expect(response.status).to eq 422
-        end
-
-        it 'doesnt create new answer' do
-          expect{
-            subject
-          }.to_not change(Answer, :count)
-        end
-      end
-    end
-
-    def do_request(options = {})
-      post "/api/v1/questions/#{question.id}/answers", { format: :json }.merge(options)
+      get '/api/v1/profiles/me', { format: :json }.merge(options)
     end
   end
 end
